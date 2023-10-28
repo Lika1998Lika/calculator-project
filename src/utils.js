0afb3e6fdbd067a91e0ae155ed58b0c67754f5e7
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-10-29 01:13:38 +0600

    refactor: delete function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
 import path from 'node:path';
 
-export const buildFixturesPath = (filepath) => path.resolve('__fixtures__', filepath);
 export const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
