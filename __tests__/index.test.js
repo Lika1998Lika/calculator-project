ef63ffdd864752313b6f9d2cff76a9db19495b4c
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-11-23 01:25:36 +0600

    refactor: update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-undef */
 import path from 'path';
 import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const stylishResult = readFile('stylish.expected.txt');
